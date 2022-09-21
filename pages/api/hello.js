57fcb648a2b1cd8bfe57e48cdbc78054c801a310
Josh <darkernight0@gmail.com>
2022-09-21 12:17:30 -0500

    started writing api method

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import https from 'https'
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+  const makeRequest = async() => {
+    https.get("https://www.knautzfamilywi.com/CareFinder-1.0.0/api/key/get", (resp) => {
+      let data = ''
+  
+      // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.log(data)
+      return data
+    });
+  
+    }).on("error", (err) => {
+      return "Error : " + err.message  
+      
+    })
+  }
+
+  console.log("test")
+
+   makeRequest().then( output => {
+    console.log("in output " + output)
+    res.status(200).json({ data: output })
+
+   })
+
+
 }
