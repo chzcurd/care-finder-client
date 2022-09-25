ecf6adba756f8bee8425cf09b7f527eecd711d46
Josh <darkernight0@gmail.com>
2022-09-25 02:23:31 -0500

    show error text if fetching data fails

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import { makeGET, proxyGET, proxyReq } from '../helpers/httpHelpers'
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { makeGET, proxyGET, proxyReq } from "../helpers/httpHelpers";
 //import styles from '../styles/Home.module.css'
-import styles from '../styles/home.module.scss'
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
+  const [token, setToken] = useState(null);
 
-  const [token, setToken] = useState(null)
-
-  const [WIHospital, setWIHospital] = useState(null)
+  const [WIHospital, setWIHospital] = useState(null);
 
   useEffect(() => {
-
     async function fetchData() {
-    let token = await makeGET("/api/key/get");
-    console.log(token)
-    token = token.xml.key;
-    setToken(token);
+      let token = await makeGET("/api/key/get");
+      console.log(token);
+      token = token.xml.key;
+      setToken(token);
     }
-    fetchData()
-
-  }, [])
+    fetchData();
+  }, []);
 
   useEffect(() => {
-
-    if (token != null){
+    if (token != null) {
       async function fetchData() {
         let resp = await makeGET("/api/hospitals/state/wi", token);
-        console.log(resp)
-        setWIHospital(resp)
+        if (resp == null) {
+          setWIHospital("Error fetching data!");
+        } else {
+          setWIHospital(resp);
         }
-        fetchData()
+      }
+      fetchData();
     }
-    
-
-  }, [token])
+  }, [token]);
 
   return (
     <div className={styles.container}>
@@ -46,17 +43,22 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Care Finder
-        </h1>
+        <h1 className={styles.title}>Welcome to Care Finder</h1>
 
         <p>Very cool 👍</p>
 
-        <p className={`${styles.tac}`}>Token:<br />{token}</p>
+        <p className={`${styles.tac}`}>
+          Token:
+          <br />
+          {token}
+        </p>
 
-        <p>/api/hospitals/state/wi:<br />{WIHospital}</p>
-
+        <p className={`${styles.tac}`}>
+          /api/hospitals/state/wi:
+          <br />
+          {WIHospital}
+        </p>
       </main>
     </div>
-  )
+  );
 }
