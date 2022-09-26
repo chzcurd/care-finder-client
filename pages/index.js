4e499688299218b9660e6a9eb622695c839dcd71
Josh <darkernight0@gmail.com>
2022-09-26 11:16:31 -0500

    print out all hospitals

--- pages/index.js
+++ pages/index.js
@@ -33,9 +33,8 @@ export default function Home() {
           let hospitalNames = [];
           itemArray.forEach((item) => {
             console.log(item);
-            hospitalNames.push(item.hospital_name);
           });
-          setWIHospital(hospitalNames);
+          setWIHospital(itemArray);
         }
       }
       fetchData();
@@ -61,11 +60,34 @@ export default function Home() {
           {token}
         </p>
 
-        <p className={`${styles.tac}`}>
-          Hospital Names:
-          <br />
-          {WIHospital?.toString()}
-        </p>
+        {WIHospital && (
+          <div className={styles.tac}>
+            <h1>Hospitals: </h1>
+            {WIHospital.map((hospital) => {
+              return (
+                <div>
+                  <h2>{hospital.hospital_name}:</h2>
+                  <p>provider_id: {hospital.provider_id}</p>
+                  <p>hospital_name: {hospital.hospital_name}</p>
+                  <p>address: {hospital.address}</p>
+                  <p>city: {hospital.city}</p>
+                  <p>state: {hospital.state}</p>
+                  <p>zip_code: {hospital.zip_code}</p>
+                  <p>county_name: {hospital.county_name}</p>
+                  <p>phone_number: {hospital.phone_number}</p>
+                  <p>hospital_type: {hospital.hospital_type}</p>
+                  <p>hospital_ownership: {hospital.hospital_ownership}</p>
+                  <p>
+                    emergency_services: {hospital.emergency_services.toString()}
+                  </p>
+                  <p>human_address: {hospital.human_address}</p>
+                  <p>latitude: {hospital.latitude}</p>
+                  <p>longitude: {hospital.longitude}</p>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </main>
     </div>
   );
