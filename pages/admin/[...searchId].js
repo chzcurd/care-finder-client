691c5eaf3ac97e9c722dab50160de560784e454c
Josh <darkernight0@gmail.com>
2022-12-08 21:29:35 -0600

    added auth to admin page

--- pages/admin/[...searchId].js
+++ pages/admin/[...searchId].js
@@ -14,7 +14,7 @@ import Link from "next/link";
 import { blue } from "@mui/material/colors";
 import Header from "../../components/header/head";
 import { StyledTextField } from "../../styles/muiStyle";
-import { useSession } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import { useForm } from "react-hook-form";
 
@@ -29,13 +29,28 @@ export default function Home(props) {
   //session
   const { data: session } = useSession();
   const [token, setToken] = useState(session?.jwt || null);
+  const [hospitals, setHospitals] = useState(null);
 
   //Search Type
   const searchType = props.searchId[0];
   const isRequired = routeLengthMap[searchType];
 
   //Search code, set the api method depending on the route that is chosen
+  useEffect(() => {}, [searchType]);
+
+  //Fetch the api key that is needed
   useEffect(() => {
+    if (session) {
+      setToken(session.jwt);
+    }
+  }, [session]);
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    // Handle form submission here
+    console.log(data);
+
     switch (searchType) {
       //add
       case "add":
@@ -51,145 +66,174 @@ export default function Home(props) {
         //setSearch("all");
         break;
     }
-  }, [searchType]);
+  };
 
-  //Fetch the api key that is needed
-  useEffect(() => {
-    if (session) {
-      setToken(session.jwt);
-    }
-  }, [session]);
+  const handleSignin = (e) => {
+    e.preventDefault();
+    signIn();
+  };
 
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    // Handle form submission here
-    console.log(data);
+  const handleSignout = (e) => {
+    e.preventDefault();
+    signOut();
   };
 
   return (
-    <div style={{ display: "grid", justifyContent: "center" }}>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
+      {session?.isAdmin !== true ? (
         <div>
-          <label>provider_id:</label>
-          <input
-            name="provider_id"
-            {...register("provider_id", {
-              required: true,
-            })}
-          />
+          <p>Please sign in as an Admin</p>
+          {session && (
+            <a href="#" onClick={handleSignout} className="btn-signin">
+              SIGN OUT
+            </a>
+          )}
+          {!session && (
+            <a href="#" onClick={handleSignin} className="btn-signin">
+              SIGN IN
+            </a>
+          )}
         </div>
-        <div>
-          <label>hospital_name:</label>
-          <input
-            name="hospital_name"
-            {...register("hospital_name", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>address:</label>
-          <input
-            name="address"
-            {...register("address", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>city:</label>
-          <input
-            name="city"
-            {...register("city", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>state:</label>
-          <input
-            name="state"
-            {...register("state", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>zip_code:</label>
-          <input
-            name="zip_code"
-            {...register("zip_code", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>county_name:</label>
-          <input
-            name="county_name"
-            {...register("county_name", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>hospital_type:</label>
-          <input
-            name="hospital_type"
-            {...register("hospital_type", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>hospital_ownership:</label>
-          <input
-            name="hospital_ownership"
-            {...register("hospital_ownership", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>emergency_services:</label>
-          <input
-            name="emergency_services"
-            type="checkbox"
-            {...register("emergency_services", {
-              required: false,
-            })}
-          />
-        </div>
-        <div>
-          <label>phone_number:</label>
-          <input
-            name="phone_number"
-            {...register("phone_number", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>latitude:</label>
-          <input
-            name="latitude"
-            {...register("latitude", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>logitude:</label>
-          <input
-            name="logitude"
-            {...register("logitude", {
-              required: true,
-            })}
-          />
-        </div>
-        <button type="submit">Submit</button>{" "}
-      </form>
+      ) : (
+        <>
+          <div style={{ display: "grid", justifyContent: "center" }}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <label>provider_id:</label>
+                <input
+                  name="provider_id"
+                  {...register("provider_id", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>hospital_name:</label>
+                <input
+                  name="hospital_name"
+                  {...register("hospital_name", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>address:</label>
+                <input
+                  name="address"
+                  {...register("address", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>city:</label>
+                <input
+                  name="city"
+                  {...register("city", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>state:</label>
+                <input
+                  name="state"
+                  {...register("state", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>zip_code:</label>
+                <input
+                  name="zip_code"
+                  {...register("zip_code", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>county_name:</label>
+                <input
+                  name="county_name"
+                  {...register("county_name", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>hospital_type:</label>
+                <input
+                  name="hospital_type"
+                  {...register("hospital_type", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>hospital_ownership:</label>
+                <input
+                  name="hospital_ownership"
+                  {...register("hospital_ownership", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>emergency_services:</label>
+                <input
+                  name="emergency_services"
+                  type="checkbox"
+                  {...register("emergency_services", {
+                    required: false,
+                  })}
+                />
+              </div>
+              <div>
+                <label>phone_number:</label>
+                <input
+                  name="phone_number"
+                  {...register("phone_number", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>latitude:</label>
+                <input
+                  name="latitude"
+                  {...register("latitude", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <label>logitude:</label>
+                <input
+                  name="logitude"
+                  {...register("logitude", {
+                    required: true,
+                  })}
+                />
+              </div>
+              <button type="submit">Submit</button>{" "}
+            </form>
+          </div>
+          <div>
+            {/* Map each hospital to an element */}
+            {hospitals &&
+              hospitals.map((hospital, index) => {
+                return (
+                  <ShowHospital
+                    hospital={hospital}
+                    hideValue={hasER}
+                    key={`ShowHospital-${hospital.provider_id}`}
+                  />
+                );
+              })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
